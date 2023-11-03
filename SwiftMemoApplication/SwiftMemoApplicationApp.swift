3192cbea45f308ad51672b0ce71591576b8d82f9
wglee0511 <ranazang@naver.com>
2023-11-03 18:21:26 +0900

    Initial Commit

---
+++ SwiftMemoApplication/SwiftMemoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMemoApplicationApp.swift
+//  SwiftMemoApplication
+//
+//  Created by racoon on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMemoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
